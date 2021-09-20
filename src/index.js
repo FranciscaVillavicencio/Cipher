379b40d7f2ddf6e8a4edf1901770fa9db58d1e5f
Francisca Villavicencio <franciscavillavicencio@MacBook-Air-de-Francisca.local>
2021-09-20 11:35:04 -0300

    actualización css html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//agregar función al botón//
+
+document.getElementById("codificar").addEventListener("click", () => {
+    alert("hellou"); 
+
+});
+
+//función para que todas las letras ingresadas sean mayusculas//
+
+
+     document.getElementById("mensaje1").addEventListener("keyup", function(){ 
+     this.value = this.value.toUpperCase();
+
+    });
