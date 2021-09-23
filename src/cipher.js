703150c099887982bb5383e1007ae39ee0225a43
Francisca Villavicencio <franciscavillavicencio@MacBook-Air-de-Francisca.local>
2021-09-23 12:32:03 -0300

    actualización funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
 const cipher = {
-  // ...
+  
+  encode: (texto, desplazamiento) => {
+    //espacio guardado para mostrar el texto cifrado//
+    let text1 = "";
+    for (let i = 0; i < texto.length; i++) {
+      //con esto todo lo ingresado se busca dentro del  codigo ascii//
+      let asciiCode = texto.charCodeAt(i);
+      // toma la posicion del codigo ascii, se transforma en abcedario normal, se le suma desplazamiento y luego se aplica el modulo de 26 y sumandole 65 se transforma nuevamente en ascii//
+      let formula = ((asciiCode - 65 + desplazamiento) % 26) + 65;
+      // hay que crear nuevo espacio (variable: espacio de memoria que guardara lo que se le "diga que contenga") para esa posición//
+      let nuevaPosicion = String.fromCharCode(formula);
+      text1 += nuevaPosicion;
+    }
+    return text1;
+  }, 
+  
+  decode: (texto, desplazamiento) => {
+    //espacio guardado para mostrar el texto cifrado//
+    let text2 = "";
+    for (let i = 0; i < texto.length; i++) {
+      //con esto todo lo ingresado se busca dentro del  codigo ascii//
+      let asciiCode = texto.charCodeAt(i);
+      // toma la posicion del codigo ascii, se transforma en abcedario normal, se le suma desplazamiento y luego se aplica el modulo de 26 y sumandole 65 se transforma nuevamente en ascii//
+      let formula2 = ((asciiCode + 65 - desplazamiento) % 26) + 65;
+      // hay que crear nuevo espacio (variable: espacio de memoria que guardara lo que se le "diga que contenga") para esa posición//
+      let nuevaPosicion2 = String.fromCharCode(formula2);
+      text2 += nuevaPosicion2;
+    }
+    return text2;
+  }
 };
 
-export default cipher;
+export default cipher; //es lo que permite exportar"
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 console.log(cipher);
 
-//agregar función al botón//
-
-document.getElementById("codificar").addEventListener("click", () => {
-    alert("hellou"); 
-
-});
-
 //función para que todas las letras ingresadas sean mayusculas//
 
+document.getElementById("mensaje1").addEventListener("keyup", function () {
+  this.value = this.value.toUpperCase();
+});
 
-     document.getElementById("mensaje1").addEventListener("keyup", function(){ 
-     this.value = this.value.toUpperCase();
+//funcion de los botones//
 
-    });
+
+document.getElementById("codificar").addEventListener("click", () => {
+  let texto = document.getElementById("mensaje1").value;
+  let desplazamiento = parseInt(document.getElementById("offset").value); //porque se le pone 10?//
+  document.getElementById("mensaje2").value = cipher.encode(
+    texto,
+    desplazamiento
+  );
+});
+
+document.getElementById("decodificar").addEventListener("click", () => {
+  let texto = document.getElementById("mensaje2").value;
+  let desplazamiento = parseInt(document.getElementById("offset").value); //porque se le pone 10?//
+  document.getElementById("mensaje2").value = cipher.decode(
+    texto,
+    desplazamiento
+  );
+});
+
+//agregar funcion para borrar input//
+
+document.getElementById("limpiar").addEventListener("click", () => {
+  alert("hellou");
+});
